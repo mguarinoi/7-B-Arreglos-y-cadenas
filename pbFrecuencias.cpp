de50a68deaab8bbc77fce00e54b90d5a86a6a497
mguarinoi <mguarinoi@unjbg.edu.pe>
2026-06-22 00:55:16 -0500

    pbFrecuencias: Solucion incial

---
+++ pbFrecuencias.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int P, n;
+    cin >> P >> n;
+
+    int freq[105] = {0};
+
+    for (int i = 0; i < n; i++) {
+        int F;
+        cin >> F;
+        freq[F]++;
+    }
+
+    for (int i = 1; i <= P; i++) {
+        cout << i << "-" << freq[i] << "\n";
+    }
+
+    return 0;
+}
