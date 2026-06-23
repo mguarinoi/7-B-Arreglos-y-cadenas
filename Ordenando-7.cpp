dd72a72be42fd59698c7744c5299c9bd766c8dc3
mguarinoi <mguarinoi@unjbg.edu.pe>
2026-06-22 20:31:17 -0500

    Ordenando-7: Ordenando letras

--- Ordenando-7.cpp
+++ Ordenando-7.cpp
@@ -8,13 +8,23 @@ int main() {
     int cantidad_letras = 0;
 
     while (cin >> palabra) {
-        for (int i = 0; palabra[i] < 'a' || palabra[i] > 'z' ? palabra[i] : 'a'; i++) {
+        for (int i = 0; ; i++) {
             if (palabra[i] == '\0') {
                 break;
             }
             if (palabra[i] >= 'a' && palabra[i] <= 'z') {
                 letras[cantidad_letras] = palabra[i];
                 cantidad_letras++;
+            }
+        }
+    }
+
+    for (int i = 0; i < cantidad_letras - 1; i++) {
+        for (int j = i + 1; j < cantidad_letras; j++) {
+            if (letras[i] > letras[j]) {
+                char temporal = letras[i];
+                letras[i] = letras[j];
+                letras[j] = temporal;
             }
         }
     }
